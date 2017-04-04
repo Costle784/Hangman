717eee0f3192aaf9cd36dd8206186e18fc142049
Curtis Ostle <curtisostle@gmail.com>
2017-04-04 15:28:19 -0400

    got letters to appear when clicked

--- main.js
+++ main.js
@@ -1,54 +1,40 @@
-console.log('connected');
 
+//targeted all jQuery popups
 let popup1 = $('#popup1');
 let popup2 = $('#popup2');
 let popup3 = $('#popup3');
 let popup4 = $('#popup4');
 let popup5 = $('#popup5');
+
+//targeted all jQuery Gameplay elements
 let letterContainer = $('#letterContainer');
 let popupContainer = $('#popupContainer');
-let gameBoard = $('#gameBoard');
+let hangmanContainer = $('#hangmanbox');
+let categorySelector = $('#selector');
+let selectButton = $('#select');
+let hangmanBox = $('#hangmanBox');
 
+//button event listeners for popup1//
 $('#chooseOwnWord').on('click', () => {popup1.hide(); popup2.slideDown()});
 $('#randomGenerate').on('click', () => {popup1.hide(); popup2.slideDown()});
-$('#startbutton').on('click', drawGameBoard);
 
+//hide all gameGamplay elements while popups take user inputs
 letterContainer.toggleClass('letterContainer')
-gameBoard.hide();
+hangmanBox.hide();
 popup2.hide();
 popup3.hide();
 popup4.hide();
 popup5.hide();
 
-function drawGameBoard () {
-  createletters();
-  // createGameSpaces();
-}
-
-function createletters() {
-  let letterArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  for (let i = 0; i <= 25; i++){
-    let div = $('<div class=letter></div>')
-    letterContainer.append(div);
-    div.html(letterArray[i]);
-  }
-    letterContainer.toggleClass('letterContainer');
-    popupContainer.hide();
-    gameBoard.show();
-}
-
-let selector = $('#selector');
-let selectButton = $('#select');
-
+//add event listener for popup2
 selectButton.on('click', chooseWordFromCategory);
-
+//stored values for popup2 categories
 let actors = ['Wesley Snipes','Steve Buscemi','Adam Sandler'];
 let movies = [];
 let rockStars = [];
-
-
+//retrieve user selected category
 function chooseWordFromCategory(){
-      let category = selector.val();
+      let category = categorySelector.val();
       switch (category) {
         case 'Actors':
           pickRandom(actors);
@@ -64,32 +50,62 @@ function chooseWordFromCategory(){
           break;
       }
 }
+//randomly select an element from the category and pass to creatGameSpaces
+var selectedWord = "";
 
+function pickRandom(array) {
+  let randomNumber = Math.floor(Math.random() * array.length);
+   selectedWord = array[randomNumber];
+  creatGameSpaces(selectedWord);
+}
+//add even listener for popup3, draw gameBoard
+$('#startbutton').on('click', drawGameBoard);
+function drawGameBoard () {
+  popup3.hide();
+  hangmanBox.fadeIn();
+  createletters();
+}
 function closePop2() {
   popup2.hide();
   popup3.slideDown();
 }
 
-function pickRandom(array) {
-  let randomNumber = Math.floor(Math.random() * array.length);
-  let selectedWord = array[randomNumber];
-  creatGameSpaces(selectedWord);
-}
-
-
+let letterArray = [];
 function creatGameSpaces(word) {
   let gameLettersContainer = $('#gameContainer');
   for (let i = 0; i < word.length; i++){
-    console.log(word[i]);
-    debugger;
     if(word[i] === " ") {
       let div = $('<div class=gamespace></div>');
       gameLettersContainer.append(div);
+      letterArray.push(div);
     }
     else {
       let div = $('<div class=gameletter></div>')
       gameLettersContainer.append(div);
       div.html(word[i]);
+      letterArray.push(div);
+    }
+  }
+}
+
+function createletters() {
+  let letterArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  for (let i = 0; i <= 25; i++){
+    let div = $('<div class=letter></div>')
+    div.on('click',checkForLetters);
+    letterContainer.append(div);
+    div.html(letterArray[i]);
+  }
+    letterContainer.toggleClass('letterContainer');
+    popupContainer.hide();
+}
+
+function checkForLetters() {
+  console.log(this);
+  $(this).removeEventListener('click', checkForLetters());
+  for (var i = 0; i < letterArray.length; i++) {
+    if(letterArray[i].html().toUpperCase() === this.innerHTML) {
+       letterArray[i].toggleClass('makevisible');
     }
   }
 }
